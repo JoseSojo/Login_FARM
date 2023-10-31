b52db2f56dbe17225d5ee2f96711be28ecd39fae
JoseSojo <jsojo346@gmail.com>
2023-10-31 16:41:38 -0400

    test 3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+#from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from src.controllers import auth_ctrl, user_ctrl
 
@@ -17,4 +17,4 @@ app.add_middleware(
 
 app.include_router(auth_ctrl.router)
 app.include_router(user_ctrl.router)
-app.mount('/static', StaticFiles(directory='static'), name='static') # static files
+#app.mount('/static', StaticFiles(directory='static'), name='static') # static files
